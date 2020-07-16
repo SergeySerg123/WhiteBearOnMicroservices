459b6d0545a49885c69b8370431d13c6182e08bd
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-16 12:27:18 +0300

    Add models
    Models

---
+++ frontend/whitebear/src/app/models/productItem/product-item.model.ts
@@ -0,0 +1,12 @@
+export interface ProductItem {
+    id: string;
+    previewImg: string;
+    name: string;
+    description: string;
+    price: number;
+    discount: number;
+    raiting: number;
+    categoryName: string;
+    brandName: string;
+    typeName: string;
+}
