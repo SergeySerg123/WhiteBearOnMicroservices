8534c56aae0f0dcf965c82dacc865ad8f80d6aac
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-17 23:50:10 +0300

    Create CategoriesRepository.cs

---
+++ backend/src/Services/Catalog/Catalog.Api/Repositories/CategoriesRepository.cs
@@ -0,0 +1,13 @@
+﻿using WhiteBear.Services.Catalog.Api.Data.Context;
+using WhiteBear.Services.Catalog.Api.Repositories.Abstract;
+using WhiteBear.Services.Catalog.Api.Repositories.Interfaces;
+
+namespace WhiteBear.Services.Catalog.Api.Repositories
+{
+    public class CategoriesRepository : BaseRepository, ICategoriesRepository
+    {
+        public CategoriesRepository(CatalogContext context) : base(context)
+        {
+        }
+    }
+}
