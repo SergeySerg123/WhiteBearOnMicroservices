47a2d0bdac263bfe3a1d6e3b6405ccf0dd9ab88e
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-17 11:57:30 +0300

    Create CategoryDTO.cs

---
+++ backend/src/Services/Catalog/Catalog.Api/Data/DTO/Category/CategoryDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using WhiteBear.Services.Catalog.Api.Data.DTO.Brand;
+
+namespace WhiteBear.Services.Catalog.Api.Data.DTO.Category
+{
+    public sealed class CategoryDTO
+    {
+        public string Name { get; set; }
+        public ICollection<BrandDTO> Brands { get; set; } 
+    }
+}
