92114e7a2510a523c97cf7bcce73a0ee9d3d6837
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-17 09:35:43 +0300

    Create CatalogContext.cs

---
+++ backend/src/Services/Catalog/Catalog.Api/Data/Context/CatalogContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WhiteBear.Services.Catalog.Api.Data.Context
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base (options)
+        {
+
+        }
+
+        
+    }
+}
