483f2d7e1cc9f516eeac712769f2f1e163c8c285
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-17 16:17:42 +0300

    Create NewProductItemDTO.cs

---
+++ backend/src/Services/Catalog/Catalog.Api/Data/DTO/ProductItem/NewProductItemDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace WhiteBear.Services.Catalog.Api.Data.DTO.ProductItem
+{
+    public sealed class NewProductItemDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double? Discount { get; set; } = 0.0;
+        public decimal Price { get; set; }
+        public int BeerType { get; set; }
+        public double Density { get; set; }
+        public string PreviewImgUrl { get; set; }
+        public string CategoryId { get; set; }
+        public string BrandId { get; set; }
+    }
+}
