41eb9d594a16907593706cf6db10d5d5aa2d15f0
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-17 15:21:15 +0300

    Create BaseService.cs

---
+++ backend/src/Services/Catalog/Catalog.Api/Services/Abstract/BaseService.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace WhiteBear.Services.Catalog.Api.Services.Abstract
+{
+    public abstract class BaseService
+    {
+        private protected readonly IMapper _mapper;
+
+        public BaseService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+    }
+}
