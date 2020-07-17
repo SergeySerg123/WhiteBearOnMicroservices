1783cff771e80b0bd2b154f225eddc348bd5251b
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-17 15:20:43 +0300

    Create BaseEntity.cs

---
+++ backend/src/Services/Catalog/Catalog.Api/Data/Entities/Abstract/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WhiteBear.Services.Catalog.Api.Data.Entities.Abstraction
+{
+    public abstract class BaseEntity
+    {
+        private DateTime _createdAt;
+
+        public BaseEntity()
+        {
+            CreatedAt = UpdatedAt = DateTime.Now;
+        }
+
+        public string Id { get; set; }
+        public DateTime CreatedAt
+        {
+            get => _createdAt;
+            set => _createdAt = (value == null || value == DateTime.MinValue) ? DateTime.Now : value;
+        }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
