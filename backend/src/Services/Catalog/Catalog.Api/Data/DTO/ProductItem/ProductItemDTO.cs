5e7b73a3050dfbc0af200e1bee46d1f239ab1208
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-17 11:57:34 +0300

    Create ProductItemDTO.cs

---
+++ backend/src/Services/Catalog/Catalog.Api/Data/DTO/ProductItem/ProductItemDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using WhiteBear.Services.Catalog.Api.Data.DTO.Brand;
+using WhiteBear.Services.Catalog.Api.Data.DTO.Category;
+using WhiteBear.Services.Catalog.Api.Data.DTO.Reaction;
+
+namespace WhiteBear.Services.Catalog.Api.Data.DTO.ProductItem
+{
+    public sealed class ProductItemDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double? Discount { get; set; } = 0.0;
+        public decimal Price { get; set; }
+        public int BeerType { get; set; }
+        public double Density { get; set; }
+        public string PreviewImgUrl { get; set; }
+        public CategoryDTO Category { get; set; }
+        public BrandDTO Brand { get; set; }
+        public ICollection<ReactionDTO> Reaction { get; set; }
+    }
+}
