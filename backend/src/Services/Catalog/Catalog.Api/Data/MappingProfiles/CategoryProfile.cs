567af8204a2f66adc5c968a59f493792991563b5
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-17 18:04:48 +0300

    Create CategoryProfile.cs

---
+++ backend/src/Services/Catalog/Catalog.Api/Data/MappingProfiles/CategoryProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WhiteBear.Services.Catalog.Api.Data.DTO.Category;
+using WhiteBear.Services.Catalog.Api.Data.Entities;
+
+namespace WhiteBear.Services.Catalog.Api.Data.MappingProfiles
+{
+    public sealed class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<CategoryDTO, Category>();
+            CreateMap<Category, CategoryDTO>();
+        }
+    }
+}
