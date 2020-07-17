9ce50b04eb3a547bdd302f1bf16c44bcc61bda1b
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-17 23:50:20 +0300

    Create CategoriesService.cs

---
+++ backend/src/Services/Catalog/Catalog.Api/Services/CategoriesService.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using WhiteBear.Services.Catalog.Api.Repositories.Interfaces;
+using WhiteBear.Services.Catalog.Api.Services.Abstract;
+
+namespace WhiteBear.Services.Catalog.Api.Services
+{
+    public class CategoriesService : BaseService
+    {
+        private readonly ICategoriesRepository _categoriesRepository;
+
+        public CategoriesService(ICategoriesRepository categoriesRepository, IMapper mapper) : base(mapper) 
+        {
+            _categoriesRepository = categoriesRepository;
+        }
+
+
+    }
+}
