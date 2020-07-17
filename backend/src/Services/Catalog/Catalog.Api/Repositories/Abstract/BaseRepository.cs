0099096021258e06e235ebebd7187bbf6be47ab8
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-17 15:21:03 +0300

    Create BaseRepository.cs

---
+++ backend/src/Services/Catalog/Catalog.Api/Repositories/Abstract/BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using WhiteBear.Services.Catalog.Api.Data.Context;
+
+namespace WhiteBear.Services.Catalog.Api.Repositories.Abstract
+{
+    public class BaseRepository
+    {
+        private protected readonly CatalogContext _context;
+
+        public BaseRepository(CatalogContext context)
+        {
+            _context = context;
+        }
+    }
+}
