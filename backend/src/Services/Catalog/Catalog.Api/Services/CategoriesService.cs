1ec8e5ffbd21b1040b3ac554fb6a02a82acd30d2
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-18 09:50:22 +0300

    Update CategoriesService.cs

--- backend/src/Services/Catalog/Catalog.Api/Services/CategoriesService.cs
+++ backend/src/Services/Catalog/Catalog.Api/Services/CategoriesService.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using System.Threading.Tasks;
+using WhiteBear.Services.Catalog.Api.Data.DTO.Category;
+using WhiteBear.Services.Catalog.Api.Data.Entities;
 using WhiteBear.Services.Catalog.Api.Repositories.Interfaces;
 using WhiteBear.Services.Catalog.Api.Services.Abstract;
 
@@ -13,6 +16,37 @@ namespace WhiteBear.Services.Catalog.Api.Services
             _categoriesRepository = categoriesRepository;
         }
 
+        public async Task<Category[]> GetCategories()
+        {
+            return await _categoriesRepository.GetCategories();
+        }
 
+        public async Task<Category> GetCategoryById(string id)
+        {
+            return await _categoriesRepository.GetCategoryItem(id);
+        }
+
+        public async Task CreateCategory(NewCategoryDTO newCategoryDTO)
+        {
+            var category = _mapper.Map<Category>(newCategoryDTO);
+            await _categoriesRepository.CreateCategory(category);
+        }
+
+        public async Task UpdateCategory(CategoryDTO categoryDTO)
+        {
+            var category = _mapper.Map<Category>(categoryDTO);
+            var oldCategory = await _categoriesRepository.GetCategoryItem(category.Id);
+
+            oldCategory.Name = category.Name;
+            oldCategory.UpdatedAt = category.CreatedAt;
+
+            await _categoriesRepository.UpdateCategory(oldCategory);
+        }
+
+        public async Task DeleteCategory(string id)
+        {
+            var oldCategory = await _categoriesRepository.GetCategoryItem(id);
+            await _categoriesRepository.DeleteCategory(oldCategory);
+        }
     }
 }
