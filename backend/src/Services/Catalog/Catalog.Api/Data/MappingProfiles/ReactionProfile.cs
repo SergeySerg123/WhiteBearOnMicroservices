5e72aa8942ba9ce54e682347bef540eebac55711
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-18 10:32:12 +0300

    Create ReactionProfile.cs

---
+++ backend/src/Services/Catalog/Catalog.Api/Data/MappingProfiles/ReactionProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WhiteBear.Services.Catalog.Api.Data.DTO.Reaction;
+using WhiteBear.Services.Catalog.Api.Data.Entities;
+
+namespace WhiteBear.Services.Catalog.Api.Data.MappingProfiles
+{
+    public class ReactionProfile : Profile
+    {
+        public ReactionProfile()
+        {
+            CreateMap<ReactionDTO, Reaction>();
+            CreateMap<Reaction, ReactionDTO>();
+            CreateMap<NewReactionDTO, Reaction>();  
+        }
+    }
+}
