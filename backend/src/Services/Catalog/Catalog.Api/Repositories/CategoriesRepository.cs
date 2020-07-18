2a716523efd294935fc9cddbeb4ceff2846548bf
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-18 09:50:09 +0300

    Update CategoriesRepository.cs

--- backend/src/Services/Catalog/Catalog.Api/Repositories/CategoriesRepository.cs
+++ backend/src/Services/Catalog/Catalog.Api/Repositories/CategoriesRepository.cs
@@ -1,13 +1,51 @@
-﻿using WhiteBear.Services.Catalog.Api.Data.Context;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using WhiteBear.Services.Catalog.Api.Data.Context;
+using WhiteBear.Services.Catalog.Api.Data.Entities;
+using WhiteBear.Services.Catalog.Api.Data.Entities.Abstraction;
 using WhiteBear.Services.Catalog.Api.Repositories.Abstract;
 using WhiteBear.Services.Catalog.Api.Repositories.Interfaces;
 
 namespace WhiteBear.Services.Catalog.Api.Repositories
 {
-    public class CategoriesRepository : BaseRepository, ICategoriesRepository
+    public class CategoriesRepository : BaseRepository<BaseEntity>, 
+        ICategoriesRepository
     {
         public CategoriesRepository(CatalogContext context) : base(context)
         {
         }
+
+        public async Task<Category[]> GetCategories()
+        {
+            return await _context.Categories
+                .Include(c => c.Brands)
+                .AsNoTracking()
+                .ToArrayAsync();
+        }
+
+        public async Task<Category> GetCategoryItem(string id)
+        {
+            return await _context.Categories
+                    .Include(c => c.ProductItems)
+                    .Include(c => c.Brands)
+                    .SingleAsync(c => c.Id == id);
+        }
+
+
+        public async Task CreateCategory(Category item)
+        {
+            await base.CreateEntity(item);
+        }
+
+        public async Task UpdateCategory(Category item)
+        {
+            await base.UpdateEntity(item);
+        }
+
+        public async Task DeleteCategory(Category item)
+        {
+            await base.DeleteEntity(item);
+        }   
     }
 }
