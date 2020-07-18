ee3cf17c5a21601ec8ca83dcc6c67d1f97c6ce58
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-18 09:50:05 +0300

    Update BaseRepository.cs

--- backend/src/Services/Catalog/Catalog.Api/Repositories/Abstract/BaseRepository.cs
+++ backend/src/Services/Catalog/Catalog.Api/Repositories/Abstract/BaseRepository.cs
@@ -1,14 +1,34 @@
-﻿using WhiteBear.Services.Catalog.Api.Data.Context;
+﻿using System.Threading.Tasks;
+using WhiteBear.Services.Catalog.Api.Data.Context;
+using WhiteBear.Services.Catalog.Api.Data.Entities.Abstraction;
 
 namespace WhiteBear.Services.Catalog.Api.Repositories.Abstract
 {
-    public class BaseRepository
+    public class BaseRepository<T> where T : BaseEntity
     {
         private protected readonly CatalogContext _context;
 
         public BaseRepository(CatalogContext context)
         {
             _context = context;
+        }
+
+        public async Task CreateEntity(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateEntity(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteEntity(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
         }
     }
 }
