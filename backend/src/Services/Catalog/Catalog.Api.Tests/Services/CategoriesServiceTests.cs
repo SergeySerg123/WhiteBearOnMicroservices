67387e428751f60fb32ff36da9a0972881c4aa29
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-21 22:01:01 +0300

    Create CategoriesServiceTests.cs

---
+++ backend/src/Services/Catalog/Catalog.Api.Tests/Services/CategoriesServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Moq;
+using WhiteBear.Services.Catalog.Api.Data.MappingProfiles;
+using WhiteBear.Services.Catalog.Api.Repositories.Interfaces;
+
+namespace Catalog.Api.Tests.Services
+{
+    class CategoriesServiceTests
+    {
+        private readonly Mock<ICategoriesRepository> _mockRepository;
+        private readonly IMapper _mapper;
+
+        public CategoriesServiceTests()
+        {
+            _mockRepository = new Mock<ICategoriesRepository>();
+
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new CategoryProfile());
+            });
+
+            _mapper = mockMapper.CreateMapper();
+        }
+    }
+}
