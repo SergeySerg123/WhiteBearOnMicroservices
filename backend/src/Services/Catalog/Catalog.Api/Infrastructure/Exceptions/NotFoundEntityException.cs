f366dcd4169cdd34db1512b6bf1d682cc7a4a345
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-21 22:34:20 +0300

    Create NotFoundEntityException.cs

---
+++ backend/src/Services/Catalog/Catalog.Api/Infrastructure/Exceptions/NotFoundEntityException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WhiteBear.Services.Catalog.Api.Infrastructure.Exceptions
+{
+    public sealed class NotFoundEntityException : Exception
+    {
+        public NotFoundEntityException(string message) : base(message)
+        {
+        }
+    }
+}
