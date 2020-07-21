0392ac36e029a7f5bbe1fa1215bdda1d9b300461
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-21 23:17:43 +0300

    Create NullPropsEntityException.cs

---
+++ backend/src/Services/Catalog/Catalog.Api/Infrastructure/Exceptions/NullPropsEntityException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WhiteBear.Services.Catalog.Api.Infrastructure.Exceptions
+{
+    public sealed class NullPropsEntityException : Exception
+    {
+        public NullPropsEntityException(string message) : base(message)
+        {
+        }
+    }
+}
