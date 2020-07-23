c90d2f4a40ea90b6e0ad9c757d3ca7330d6dd313
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-23 23:34:36 +0300

    Create new-product.ts

---
+++ frontend/whitebear/src/app/models/product/new-product.ts
@@ -0,0 +1,11 @@
+export interface NewProduct {
+    name: string;
+    description: string;
+    discount: number;
+    price: number;
+    beerType: number;
+    density: number;
+    previewImg: string;
+    categotyId: string;
+    brandId: string;
+}
