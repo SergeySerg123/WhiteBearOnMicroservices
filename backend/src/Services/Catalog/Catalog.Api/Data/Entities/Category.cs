7db41e83e673055787c14ddfbf61bc2b53b5f563
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-28 10:03:42 +0300

    Update Category.cs

--- backend/src/Services/Catalog/Catalog.Api/Data/Entities/Category.cs
+++ backend/src/Services/Catalog/Catalog.Api/Data/Entities/Category.cs
@@ -7,9 +7,10 @@ namespace WhiteBear.Services.Catalog.Api.Data.Entities
     public sealed class Category : BaseEntity
     {
         [Required]
+        [MinLength(2)]
         public string Name { get; set; }
         public string ProductItemId { get; set; }
-        public ICollection<ProductItem> ProductItems { get; set; } 
+        public ICollection<Product> ProductItems { get; set; } 
         public string BrandId { get; set; }
         public ICollection<Brand> Brands { get; set; }
     }
