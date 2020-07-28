24e3402906f61d76fc8f3091f965b2100dd05fda
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-28 10:03:39 +0300

    Update Brand.cs

--- backend/src/Services/Catalog/Catalog.Api/Data/Entities/Brand.cs
+++ backend/src/Services/Catalog/Catalog.Api/Data/Entities/Brand.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using WhiteBear.Services.Catalog.Api.Data.Entities.Abstraction;
 
 namespace WhiteBear.Services.Catalog.Api.Data.Entities
 {
     public sealed class Brand : BaseEntity
     {
+        [Required]
+        [MinLength(2)]
         public string Name { get; set; }
         public string ProductItemId { get; set; }
-        public ICollection<ProductItem> ProductItems { get; set; }
+        public ICollection<Product> ProductItems { get; set; }
+        [Required]
         public string CategoryId { get; set; }
         public Category Category { get; set; }
     }
