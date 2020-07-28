bcf28fbebba51acfd8fab93bd0c61ba077a917e2
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-28 10:03:53 +0300

    Update Reaction.cs

--- backend/src/Services/Catalog/Catalog.Api/Data/Entities/Reaction.cs
+++ backend/src/Services/Catalog/Catalog.Api/Data/Entities/Reaction.cs
@@ -1,11 +1,13 @@
-﻿using WhiteBear.Services.Catalog.Api.Data.Entities.Abstraction;
+﻿using System.ComponentModel.DataAnnotations;
+using WhiteBear.Services.Catalog.Api.Data.Entities.Abstraction;
 
 namespace WhiteBear.Services.Catalog.Api.Data.Entities
 {
     public sealed class Reaction : BaseEntity
     {
         public int Value { get; set; }
+        [Required]
         public string ProductItemId { get; set; }
-        public ProductItem ProductItem { get; set; }
+        public Product ProductItem { get; set; }
     }
 }
