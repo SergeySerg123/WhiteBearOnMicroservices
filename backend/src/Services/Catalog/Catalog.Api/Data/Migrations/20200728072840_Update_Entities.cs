3d71a40987dd65ccce363ab817748e4d767a5838
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-28 16:11:25 +0300

    Create 20200728072840_Update_Entities.cs

---
+++ backend/src/Services/Catalog/Catalog.Api/Data/Migrations/20200728072840_Update_Entities.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WhiteBear.Services.Catalog.Api.Data.Migrations
+{
+    public partial class Update_Entities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reactions_ProductItems_ProductItemId1",
+                table: "Reactions");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductItemId1",
+                table: "Reactions",
+                newName: "ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Reactions_ProductItemId1",
+                table: "Reactions",
+                newName: "IX_Reactions_ProductId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductItemId",
+                table: "Reactions",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ProductItems",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryId",
+                table: "ProductItems",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BrandId",
+                table: "ProductItems",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Brands",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryId",
+                table: "Brands",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reactions_ProductItems_ProductId",
+                table: "Reactions",
+                column: "ProductId",
+                principalTable: "ProductItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reactions_ProductItems_ProductId",
+                table: "Reactions");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "Reactions",
+                newName: "ProductItemId1");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Reactions_ProductId",
+                table: "Reactions",
+                newName: "IX_Reactions_ProductItemId1");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductItemId",
+                table: "Reactions",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ProductItems",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryId",
+                table: "ProductItems",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BrandId",
+                table: "ProductItems",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Brands",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryId",
+                table: "Brands",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reactions_ProductItems_ProductItemId1",
+                table: "Reactions",
+                column: "ProductItemId1",
+                principalTable: "ProductItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
