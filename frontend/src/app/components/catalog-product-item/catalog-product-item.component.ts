daeb099aa41a327dd67c97b2a68e27162ce18305
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-30 00:33:28 +0300

    Update catalog-product-item.component.ts

--- frontend/src/app/components/catalog-product-item/catalog-product-item.component.ts
+++ frontend/src/app/components/catalog-product-item/catalog-product-item.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { faStar, faWineBottle, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { CardService } from 'src/app/services/card.service';
+import { Product } from 'src/app/models/product/product';
 
 @Component({
   selector: 'app-catalog-product-item',
@@ -8,12 +9,13 @@ import { CardService } from 'src/app/services/card.service';
   styleUrls: ['./catalog-product-item.component.scss']
 })
 export class CatalogProductItemComponent implements OnInit {
+  
+  @Input() public product: Product = null;
+  
   public faStar = faStar;
   public faWineBottle = faWineBottle;
   public faMinus = faMinus;
   public faPlus = faPlus;
-
-  public img = "../../../assets/pics/blanc.png";
 
   constructor(
     private cardService: CardService
