20138f14d471be21ce0d9f906d54edf8b208cb54
SergeySerg123 <sergey_chizhik@ukr.net>
2020-07-30 00:33:50 +0300

    Update products.service.ts

--- frontend/src/app/services/products.service.ts
+++ frontend/src/app/services/products.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpInternalService } from './http-internal.service';
 import { environment } from 'src/environments/environment';
 import { Product } from '../models/product/product';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +16,21 @@ export class ProductsService {
 
   public getProducts() {
     return this.httpService.getFullRequest(this.baseUrl + '/api/products/items');
+  }
+
+  public getProduct(id: string) {
+    return this.httpService.getFullRequest(this.baseUrl + '/api/products/items/' + id);
+  }
+
+  public createProduct(product: Product) {
+    return this.httpService.postFullRequest(this.baseUrl + '/api/products/items', product);
+  }
+
+  public updateProduct(product: Product) {
+    return this.httpService.putFullRequest(this.baseUrl + '/api/products/items', product);
+  }
+
+  public deleteProduct(id: string) {
+    return this.httpService.deleteFullRequest(this.baseUrl + '/api/products/items/' + id);
   }
 }
