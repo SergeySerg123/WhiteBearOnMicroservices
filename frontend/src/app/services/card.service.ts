96c810234179b2c6ef5d8842583ee1d2f6380c87
SergeySerg123 <sergey_chizhik@ukr.net>
2020-08-01 12:09:32 +0300

    Update card.service.ts

--- frontend/src/app/services/card.service.ts
+++ frontend/src/app/services/card.service.ts
@@ -2,25 +2,28 @@ import { Injectable } from '@angular/core';
 import { Product } from '../models/product/product';
 import { Card } from '../models/card/card';
 import { CardItem } from '../models/card/card-item';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardService {
-  private card: Card = null;
+  public card$: BehaviorSubject<Card> = null;
 
   constructor() { }
 
   public addToCard(product: Product, quantity: number) {
     let newItem: CardItem = {product, quantity};
+    var card = this.card$.value;
 
-    let item = this.card.items.find((item) => item.product.id === product.id);
+    let item = card.items.find((item) => item.product.id === product.id);
     
     if(!item === null && !item === undefined) {
       item.quantity += quantity;
     } else {
-      this.card.items.push(newItem);
+      card.items.push(newItem);
     }    
+    this.card$.next(card);
   }
 
   public deleteFromCard(product: Product) {
