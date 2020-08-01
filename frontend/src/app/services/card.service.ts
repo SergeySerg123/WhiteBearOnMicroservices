771c3bc1dcb777b63d8da7e62dcbb5227b51d676
SergeySerg123 <sergey_chizhik@ukr.net>
2020-08-01 11:37:32 +0300

    Update card.service.ts

--- frontend/src/app/services/card.service.ts
+++ frontend/src/app/services/card.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product/product';
+import { Card } from '../models/card/card';
+import { CardItem } from '../models/card/card-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardService {
+  private card: Card = null;
 
   constructor() { }
+
+  public addToCard(product: Product, quantity: number) {
+    let newItem: CardItem = {product, quantity};
+    this.card.items.push(newItem);
+  }
+
+  public deleteFromCard(product: Product) {
+    this.card.items = this.card.items.filter((item) => 
+      item.product.id !== product.id);
+  }
 }
