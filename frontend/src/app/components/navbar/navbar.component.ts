d2c9a78efbf26458608bcfa1ef5cffd09731a9e9
SergeySerg123 <sergey_chizhik@ukr.net>
2020-08-01 12:09:27 +0300

    Update navbar.component.ts

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { faBars, faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { CardService } from 'src/app/services/card.service';
+import { CardItem } from 'src/app/models/card/card-item';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
   public faBars = faBars;
   public faSearch = faSearch;
   public faShoppingCart = faShoppingCart;
@@ -14,11 +17,20 @@ export class NavbarComponent implements OnInit {
   public isOpenedMenu: boolean = false;
   public isOpenedCard: boolean = false;
 
-  public items = [];
+  public items: CardItem[] = null;
 
-  constructor() { }
+  constructor(
+    private cardService: CardService
+  ) { }
 
   ngOnInit(): void {
+    this.cardService.card$.subscribe(card => {
+      this.items = card.items;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.cardService.card$.unsubscribe();
   }
 
   public get hasItemsInCard() {
