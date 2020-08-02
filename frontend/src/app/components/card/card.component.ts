0c73a698962e5db812c49c6e36493fd7ada62165
SergeySerg123 <sergey_chizhik@ukr.net>
2020-08-03 00:12:34 +0300

    card

--- frontend/src/app/components/card/card.component.ts
+++ frontend/src/app/components/card/card.component.ts
@@ -1,23 +1,50 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit, Input, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { faTimes, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+import { CardService } from 'src/app/services/card.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { CardItem } from 'src/app/models/card/card-item';
+import { Product } from 'src/app/models/product/product';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit, OnDestroy {
   public faTimes = faTimes;
+
 
   @Input() public isOpenedCard: boolean = false;
   @Output() public toggle: EventEmitter<void> = new EventEmitter<void>();
 
-  constructor() { }
+  public items: CardItem[] = new Array<CardItem>();
+  
+  public unsubscribe$ = new Subject<any>();
+
+  constructor(
+    private cardService: CardService
+  ) { }
 
   ngOnInit(): void {
+    this.cardService.card$
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(card => {
+        this.items = card.items;
+        console.log(this.items);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   public toggleCard() {
     this.toggle.emit();
+  }
+
+  public deleteFromCard(product: Product) {
+    this.cardService.deleteFromCard(product);
   }
 }
