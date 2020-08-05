1c7e908b72ce54aac47c8c3f3dd36310a01df2ba
SergeySerg123 <sergey_chizhik@ukr.net>
2020-08-05 20:25:41 +0300

    close dialod func

--- frontend/src/app/components/bottle-dialog/bottle-dialog.component.ts
+++ frontend/src/app/components/bottle-dialog/bottle-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Product } from 'src/app/models/product/product';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { DialogService } from 'src/app/services/dialog.service';
 
 @Component({
   selector: 'app-bottle-dialog',
@@ -8,15 +10,20 @@ import { Product } from 'src/app/models/product/product';
   styleUrls: ['./bottle-dialog.component.scss']
 })
 export class BottleDialogComponent implements OnInit {
+  public faTimes = faTimes;
+
   public product: Product = null;
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) private data: Product
+    @Inject(MAT_DIALOG_DATA) private data: Product,
+    private dialogRef: MatDialogRef<BottleDialogComponent>
   ) { }
 
   ngOnInit(): void {
     this.product = this.data;
   }
 
-  
+  public closeDialog() {
+    this.dialogRef.close(false);
+  }
 }
