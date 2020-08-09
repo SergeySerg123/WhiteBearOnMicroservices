dc2040ce277e66da68f63496379ef5a353a470d5
SergeySerg123 <sergey_chizhik@ukr.net>
2020-08-09 12:58:12 +0300

    Update bottle.service.ts

--- frontend/src/app/services/bottle.service.ts
+++ frontend/src/app/services/bottle.service.ts
@@ -16,14 +16,67 @@ export class BottleService implements OnInit, OnDestroy {
 
   constructor(private httpService: HttpInternalService) { }
   
-  ngOnInit(): void {
-    this.httpService.getFullRequest(this.baseUrl + '/api/bottles')
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe((resp) => this.bottles = resp.body as Bottle[]);
+  ngOnInit(): void {    
   }
 
   ngOnDestroy(): void {
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
+  }
+
+  public loadBottles() {
+    this.httpService.getFullRequest(this.baseUrl + '/api/bottles')
+    .pipe(takeUntil(this.unsubscribe$))
+    .subscribe((resp) => {
+      this.bottles = resp.body as Bottle[];
+    } );
+  }
+
+  public calcBottles(quantity: number): Bottle[] {
+    let bottle = this.bottles.filter( (bottle) => bottle.capacity === quantity );
+    
+    if (bottle !== null && bottle !== undefined) {
+      return  [...bottle];
+    }
+
+    let maxCapacityBottle = this.calcMaxCapacityBottle();
+
+    let countBottleWithMaxCapacity = Math.floor(quantity/maxCapacityBottle.capacity);
+    let lastBottleCapacity = quantity - (maxCapacityBottle.capacity * countBottleWithMaxCapacity);
+
+    return this.selectBottles(countBottleWithMaxCapacity, maxCapacityBottle.capacity, lastBottleCapacity);
+  }
+
+  private calcMaxCapacityBottle(): Bottle {
+    return this.bottles.reduce((prev, current) => {
+      if (+current.capacity > +prev.capacity) {
+        return current;
+      } else {
+          return prev;
+      }
+    });
+  }
+
+  private selectBottles(countBottleWithMaxCapacity: number, 
+    maxCapacityBottle: number, lastBottleCapacity: number): Bottle[] {
+    
+     let bottles: Bottle[] = [];
+    
+     for(let i = 0; i < countBottleWithMaxCapacity; i++) {
+      bottles.push(
+        this.bottles.find( 
+          (bottle) => bottle.capacity === maxCapacityBottle)
+      );
+    }
+
+    
+    if (lastBottleCapacity !== 0) {
+      bottles.push(
+        this.bottles.find( 
+          (bottle) => bottle.capacity === lastBottleCapacity)
+      );
+    }
+    
+    return bottles;
   }
 }
