aeef0f88a37478f44a56ccfde40d72ca6c167dd3
SergeySerg123 <sergey_chizhik@ukr.net>
2020-08-09 12:56:58 +0300

    Update card-item.ts

--- frontend/src/app/models/card/card-item.ts
+++ frontend/src/app/models/card/card-item.ts
@@ -1,6 +1,9 @@
 import { Product } from '../product/product';
+import { Bottle } from '../bottle/bottle';
 
 export interface CardItem {
     product: Product;
     quantity: number;
+    bottles: Bottle[];
+    totalBottlesPrice: number;
 }
