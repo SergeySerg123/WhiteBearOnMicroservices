c2e99c4efed273ce355d77e1801fa43228882319
SergeySerg123 <sergey_chizhik@ukr.net>
2020-08-09 12:56:05 +0300

    Create Bottle.cs

---
+++ backend/src/Services/Catalog/Catalog.Api/Data/Entities/Bottle.cs
@@ -0,0 +1,10 @@
+﻿using WhiteBear.Services.Catalog.Api.Data.Entities.Abstraction;
+
+namespace WhiteBear.Services.Catalog.Api.Data.Entities
+{
+    public class Bottle : BaseEntity
+    {
+        public double Price { get; set; }
+        public double Capacity { get; set; }
+    }
+}
